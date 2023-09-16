dbe49b429b1e2438d8f7c3a0e1b54a3c0c5ffd20
Cgs1208 <charangs157@gmail.com>
2023-09-16 23:43:17 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello from React"
+); //this create element return an react element which is inturn a js object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
